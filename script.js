8b1e3c62687f961b6752858f36669c8da5d5f962
Arpitpadhy <127716002+Arpitpadhy@users.noreply.github.com>
2025-10-08 18:41:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function minDate(dates) {
   //write you code here
+	let min = dates[0]; // Start with the first date as minimum
+
+  for (let i = 1; i < dates.length; i++) {
+    if (dates[i] < min) {
+      min = dates[i]; // Update min if a smaller date is found
+    }
+  }
+
+  return min;
 }
+	
+
 
 // Do not change the code
 
